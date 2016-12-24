1541b9216cc15f7607d6faa6f40d95c23c00f780
Dinesh Krishnan <dinaki08tt@gmail.com>
2016-12-24 16:21:03 +0530

    localStorage added in Create Category screen

--- WebContent/js/category_mock.js
+++ WebContent/js/category_mock.js
@@ -35,6 +35,7 @@ function getAllCategory(){
 
 function createCategory(name,gender,type){
 	console.log(name+":"+gender+":"+type);
+	localSaveCategory(name,gender,type);
 }
 
 function updateCategory(name,gender,type){
@@ -44,3 +45,50 @@ function updateCategory(name,gender,type){
 function deleteCategory(name,gender,type){
 	console.log(name+":"+gender+":"+type);
 }
+
+var key = 'category';
+
+function clearLocalStorage(){
+	$.localStorage.remove(key);
+}
+
+function localSaveCategory(name,gender,type){
+	//$.localStorage.remove(key);
+var obj = new Category(name, gender, type);
+
+var ret = $.localStorage.get(key);
+if(ret!=null)
+{
+	ret[ret.length] = obj;
+}else{
+	ret = new Array();
+	ret[0] = obj;
+}	
+	
+$.localStorage.set(key,JSON.stringify(ret));
+	
+	var ret = $.localStorage.get(key);
+	var instance = ret[0];
+	console.log("Object in localStorage: "+instance.name);
+}
+
+function Category(name, gender, type){
+	this.name = name;
+	this.gender = gender;
+	this.type = type;
+}
+
+function readCategory(name){
+	
+	var cats = $.localStorage.get(key);
+	console.log(cats);
+
+	 $.each(cats, function(index,cat) {
+		    if(cat.name === 'cadet'){
+		    	console.log("present in local: "+cat)
+		    	return cat;
+		    }
+	 });
+
+}
+
